89f43ae158473b18634812ae8049e3addd4c6166
NateM135 <melwaninate@gmail.com>
2022-01-15 21:41:17 -0800

    files created for compare are now in tmp/

--- backend/compare.py
+++ backend/compare.py
@@ -8,13 +8,13 @@ def compareImages(filename1: str, filename2: str):
     f_img = filename1
     img = Image.open(f_img)
     img = img.resize((45,39))
-    img.save('test1.png')
+    img.save('../tmp/compare1.png')
 
     f_img = filename2
     img = Image.open(f_img)
     img = img.resize((45,39))
-    img.save('test2.png')
+    img.save('../tmp/compare2.png')
 
-    diff_percent = image_diff_percent("test1.png", "test2.png")
+    diff_percent = image_diff_percent("../tmp/compare1.png", "../tmp/compare2.png")
 
     return diff_percent
