13ca3d37615eb1bedac102e0a1dcba6d1be370a5
NateM135 <melwaninate@gmail.com>
2022-01-15 16:35:43 -0800

    ascript gets coords and invokes coord to make images of people faces

---
+++ script.py
@@ -0,0 +1,40 @@
+from crop2 import Crop
+import uuid
+
+#export GOOGLE_APPLICATION_CREDENTIALS="/home/natem135/Downloads/rosehack2022-f178de763729.json"
+
+def detect_faces(path):
+    """Detects faces in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    for face in faces:
+        # print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        # print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        # print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = ([(vertex.x, vertex.y) for vertex in face.bounding_poly.vertices])
+        img = Crop(vertices[0][0], vertices[0][1], vertices[1][0], vertices[1][1], vertices[2][0], vertices[2][1], vertices[3][0], vertices[3][1], "ccdcteam.png", f"tmp/{uuid.uuid4().hex}.png")
+        img.generate_crop()
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+detect_faces('/home/natem135/Downloads/FaMousse/ccdcteam.png')
